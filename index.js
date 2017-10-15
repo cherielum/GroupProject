e19f2b7cbc2ee5fcb9bf15eeaa2e0efc3f39b7d3
Tina Bryson <tinabryson@tinas-mbp.attlocal.net>
2017-10-15 18:42:55 -0400

    added function for Ariana and Barbra

--- index.js
+++ index.js
@@ -1,32 +1,3 @@
-$(function() {
-
-	$('.dropdown-menu').on('click', 'input', function(){
-		var chosenCelebrity = $(this).val();
-		console.log(chosenCelebrity);
-		var url = "https://api.themoviedb.org/3/search/person?api_key=e09be2ac4d045c2e602d7bf0280ce9ff&language=en-US&query="+ chosenCelebrity +"&page=1&include_adult=false";
-		$.get(url, function(data){
-			console.log(data);
-			var celebretyId = data.results[0].id;
-			console.log(celebretyId);
-		});
-	})
-
-});
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
 	var map;
 	var mapDiv = document.getElementById('map');
@@ -120,6 +91,16 @@ $(function() {
 		})
 	})
 
+	var getBSId = $('.BS').on('click', 'input', function(e){
+			var GetIdUrl = "https://api.themoviedb.org/3/search/person?api_key=e09be2ac4d045c2e602d7bf0280ce9ff&language=en-US&query=barbra%20streisand&page=1&include_adult=false";
+			$.get(GetIdUrl, function(data){
+					var celebId = JSON.stringify((data.results[0]["id"]));
+			var getBirthPlaceUrl = "https://api.themoviedb.org/3/person/"+ celebId + "?api_key=e09be2ac4d045c2e602d7bf0280ce9ff&language=en-US";
+			$.get(getBirthPlaceUrl, function(data){
+					console.log(data.place_of_birth)
+			});
+		})
+	})
+
 
 });
-
