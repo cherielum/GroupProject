3e3c7b3e9a353aae32623176f8257dc6dd36c815
Tina Bryson <tinabryson@tinas-mbp.attlocal.net>
2017-10-15 16:52:25 -0400

    updated class and function for Taylor Swift

--- index.js
+++ index.js
@@ -36,4 +36,15 @@ $(function() {
     })
 	})
 
+	var getBieberId = $('.TS').on('click', 'input', function(e){
+			var GetIdUrl = "https://api.themoviedb.org/3/search/person?api_key=e09be2ac4d045c2e602d7bf0280ce9ff&language=en-US&query=taylor%20swift&page=1&include_adult=false";
+			$.get(GetIdUrl, function(data){
+					var celebId = JSON.stringify((data.results[0]["id"]));
+			var getBirthPlaceUrl = "https://api.themoviedb.org/3/person/"+ celebId + "?api_key=e09be2ac4d045c2e602d7bf0280ce9ff&language=en-US";
+			$.get(getBirthPlaceUrl, function(data){
+					console.log(data.place_of_birth)
+			});
+	})
+})
+
 });
