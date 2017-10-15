dbf8af6c6383d4fe51f56a3d581b11e273462b62
Tina Bryson <tinabryson@tinas-mbp.attlocal.net>
2017-10-15 13:18:08 -0400

    added functions for getting birthplace and changed getting ID function

--- index.js
+++ index.js
@@ -15,10 +15,30 @@ console.log('trish');
 $(function() {
 
 	$('.openingImage').delay(3000).fadeOut(300)
-}); 
+});
 
 $(function() {
 
 	$('#firstDiv').delay(3000).fadeOut(300)
-}); 
+});
 
+
+$(function() {
+		var jbieber = $(".JB").on('click','input', function(){
+			var bieberIsSelected = $(this).val();
+			var url = "https://api.themoviedb.org/3/person/150810?&language=en-US&api_key=e09be2ac4d045c2e602d7bf0280ce9ff"
+			$.get(url, function(data){
+				console.log(data.place_of_birth)
+			})
+		})
+//If we make the selection an input text box then the search should do the filtering for us and come up with the correct person
+    var newStar = $('.JB').on('click', 'input', function(){
+        var chosenCelebrity = $(this).val("Justin Bieber");
+        var url = "https://api.themoviedb.org/3/search/person?api_key=e09be2ac4d045c2e602d7bf0280ce9ff&language=en-US&query=" + chosenCelebrity + "&page=1&include_adult=false";
+        $.get(url, function(data){
+            console.log(data.results[0].id)
+        });
+    })
+
+
+});
