8c74d70e541240369e35fcea02d309b7802a34b3
Tina Bryson <tinabryson@tinas-mbp.attlocal.net>
2017-10-15 15:59:21 -0400

    updated code that gets the celebrity ID

--- index.js
+++ index.js
@@ -24,19 +24,22 @@ $(function() {
 
 
 $(function() {
-		var jbieber = $(".JB").on('click','input', function(){
-			var bieberIsSelected = $(this).val();
-			var url = "https://api.themoviedb.org/3/person/150810?&language=en-US&api_key=e09be2ac4d045c2e602d7bf0280ce9ff"
-			$.get(url, function(data){
-				console.log(data.place_of_birth)
-			})
-		})
-//If we make the selection an input text box then the search should do the filtering for us and come up with the correct person
-    var newStar = $('.JB').on('click', 'input', function(){
-        var chosenCelebrity = $(this).val("Justin Bieber");
-        var url = "https://api.themoviedb.org/3/search/person?api_key=e09be2ac4d045c2e602d7bf0280ce9ff&language=en-US&query=" + chosenCelebrity + "&page=1&include_adult=false";
+		// var jbieber = $(".JB").on('click','input', function(){
+		// 	var bieberIsSelected = $(this).val();
+		// 	var url = "https://api.themoviedb.org/3/person/150810?&language=en-US&api_key=e09be2ac4d045c2e602d7bf0280ce9ff"
+		// 	$.get(url, function(data){
+		// 		console.log(data.place_of_birth)
+		// 	})
+		// })
+
+
+    var newStar = $('.JB').on('click', 'input', function(e){
+        //var chosenCelebrity = $(this).val("Justin Bieber");
+				//console.log(chosenCelebrity)
+				//e.preventDefault();
+        var url = "https://api.themoviedb.org/3/search/person?api_key=e09be2ac4d045c2e602d7bf0280ce9ff&language=en-US&query=justin%20bieber&page=1&include_adult=false";
         $.get(url, function(data){
-            console.log(data.results[0].id)
+            var celebId = console.log(data.results[0]["id"])
         });
     })
 
